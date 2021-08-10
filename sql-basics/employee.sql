cd9e328580a9142c72b97be0bd305e7243450e54
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-10 16:05:36 -0600

    finished employee

---
+++ sql-basics/employee.sql
@@ -0,0 +1,17 @@
+SELECT first_name, last_name FROM employee
+WHERE city = 'Calgary';
+
+SELECT MAX(birth_date) 
+FROM employee;
+
+SELECT MIN(birth_date) 
+FROM employee;
+
+SELECT * FROM employee
+WHERE first_name = 'Nancy' AND last_name = 'Edwards';
+
+SELECT * FROM employee
+WHERE reports_to = 2;
+
+SELECT COUNT(city) FROM employee
+Where city = 'Lethbridge';
